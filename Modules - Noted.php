92e03f6512eadfc95c6ddcb927c696c4dbeedff2
ogisetiawan <ogisetiawan21@gmail.com>
2020-10-03 15:05:36 +0700

    docs: Structure HTML5

---
+++ Modules - Noted.php
@@ -0,0 +1,20 @@
+<?php
+//@ SERVER & CLIENT
+//~ WEB SERVER
+//? Informasi yang tersimpan tapi web ini lebih ke rule dan fungsi dari aplikasi tersebut
+//~ HTTP SERVER
+//? Software yang nerima request (Hypertext Transfer Protocol / HTTP) *Apache
+//~ DNS Server
+//? Nama domain dari IP Address
+//~ CLIENT
+//? Perangkat yang request suatu response ke dalam server *Browser
+
+//@ ANATOMI WEBSITE
+//? HTML, CSS, JAVASCRIPT
+
+//@ STRUCTURE HTML
+//?             <html> * element
+//?      <head>         <body>
+//? <title>  <meta>   <p>   <h1> * content from element  
+//? <p data-*=''> * attribute 
+
