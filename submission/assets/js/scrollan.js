b69a323cdfdcf88201e67aaa995da92fd4bfd063
ogisetiawan <ogisetiawan21@gmail.com>
2020-10-19 20:54:49 +0700

    feat: Adding chart canvasjs

--- submission/assets/js/scrollan.js
+++ submission/assets/js/scrollan.js
@@ -1,21 +1,18 @@
-(function () {
+var section = document.querySelectorAll(".section"); //get all class yang section
+var sections = {};
+var i = 0;
 
-    var section = document.querySelectorAll(".section"); //get all class yang section
-    var sections = {};
-    var i = 0;
+Array.prototype.forEach.call(section, function (e) {
+    sections[e.id] = e.offsetTop; //section adlah sma 
+});
 
-    Array.prototype.forEach.call(section, function (e) {
-        sections[e.id] = e.offsetTop; //section adlah sma 
-    });
-
-    // event untuk scroll
-    window.onscroll = function () {
-        var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop; // cek posisi scroll
-        for (i in sections) { // loop section class
-            if (sections[i] <= scrollPosition) {  // kalo kurang dari posisi 
-                document.querySelector('.active').setAttribute('class', ' ');
-                document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active'); // set class pada menu 
-            }
+// event untuk scroll
+window.onscroll = function () {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop; // cek posisi scroll
+    for (i in sections) { // loop section class
+        if (sections[i] <= scrollPosition) {  // kalo kurang dari posisi 
+            document.querySelector('.active').setAttribute('class', ' ');
+            document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active'); // set class pada menu 
         }
-    };
-})();
+    }
+};
